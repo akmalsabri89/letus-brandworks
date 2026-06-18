c92ab7a7075947bb9dd4dad97847b27160dcf5a5
Akmal <akmal.kreation@gmail.com>
2026-06-18 20:07:50 +0800

    feat: add defineLocations for all document types in Presentation Tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { presentationTool } from 'sanity/presentation'
+import { presentationTool, defineLocations } from 'sanity/presentation'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './src/sanity/schemas'
 
@@ -19,6 +19,51 @@ export default defineConfig({
       previewUrl: {
         origin: previewOrigin,
         draftMode: { enable: '/api/draft-mode/enable' },
+      },
+      resolve: {
+        locations: {
+          service: defineLocations({
+            select: { name: 'name', showOnHomepage: 'showOnHomepage' },
+            resolve: (doc) => ({
+              locations: [
+                { title: doc?.name ?? 'Service', href: '/services' },
+                ...(doc?.showOnHomepage ? [{ title: 'Homepage', href: '/' }] : []),
+              ],
+            }),
+          }),
+          testimonial: defineLocations({
+            select: { name: 'name' },
+            resolve: (doc) => ({
+              locations: [{ title: doc?.name ?? 'Testimonial', href: '/' }],
+            }),
+          }),
+          caseStudy: defineLocations({
+            select: { title: 'title', slug: 'slug.current', featured: 'featured' },
+            resolve: (doc) => ({
+              locations: [
+                { title: doc?.title ?? 'Case Study', href: `/works/${doc?.slug}` },
+                ...(doc?.featured ? [{ title: 'Homepage', href: '/' }] : []),
+              ],
+            }),
+          }),
+          post: defineLocations({
+            select: { title: 'title', slug: 'slug.current' },
+            resolve: (doc) => ({
+              locations: [
+                { title: doc?.title ?? 'Post', href: `/blogs/${doc?.slug}` },
+              ],
+            }),
+          }),
+          siteSettings: defineLocations({
+            select: {},
+            resolve: () => ({
+              locations: [
+                { title: 'Homepage', href: '/' },
+                { title: 'Contact', href: '/contact' },
+              ],
+            }),
+          }),
+        },
       },
     }),
     structureTool({
